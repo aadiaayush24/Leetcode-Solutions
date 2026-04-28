d741a595f60557c326202b24a55c6bc5df2e3b10
Aadish Aayush <48391351+aadiaayush24@users.noreply.github.com>
2026-04-29 02:11:03 +0530

    Time: 3 ms (14.06%) | Memory: 43.1 MB (33.77%) - LeetSync

---
+++ 410-split-array-largest-sum/split-array-largest-sum.java
@@ -0,0 +1,33 @@
+class Solution {
+    public boolean isSumPossibleInLTEKSub(int[] nums, int sum, int k) {
+        int count=1;
+        int currSum=0;
+        for (int i: nums) {
+            if (i>sum) return false;
+            else if (currSum+i <= sum) {
+                currSum += i;
+            }
+            else {
+                currSum=i;
+                count++;
+                if (count>k) return false;
+            }
+        }
+        return true;
+    }
+    public int splitArray(int[] nums, int k) {
+        int left= Arrays.stream(nums).max().getAsInt();
+        int right=Arrays.stream(nums).sum();
+
+        while (left<right) {
+            int mid = (left + (right-left)/2);
+            if (isSumPossibleInLTEKSub(nums, mid, k)) {
+                right=mid;
+            }
+            else {
+                left=mid+1;
+            }
+        }
+        return left;
+    }
+}
