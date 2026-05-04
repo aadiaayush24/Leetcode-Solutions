314b1eba0341129ca27e3393ac4ab118e6feb883
Aadish Aayush <48391351+aadiaayush24@users.noreply.github.com>
2026-05-05 00:33:27 +0530

    Time: 0 ms (100.00%) | Memory: 43.9 MB (16.04%) - LeetSync

--- 48-rotate-image/rotate-image.java
+++ 48-rotate-image/rotate-image.java
@@ -1,20 +1,22 @@
 class Solution {
     public void rotate(int[][] matrix) {
+        // Interchange row order (last row becomes first, second last becomes second etc)
         int n = matrix.length;
+        for (int i=0; i<n/2; i++) {
+            int[] temp = new int[n];
+            temp=matrix[i];
+            matrix[i]=matrix[n-1-i];
+            matrix[n-1-i]=temp;
+        }
+        // 
         int t;
-        for (int i =0; i < n; i++)  {
-            for (int j = i+1; j<n; j++)   {
+        for (int i=0; i<n; i++) {
+            for (int j=i+1; j<n; j++) {
                 t = matrix[i][j];
-                matrix[i][j] = matrix[j][i];
+                matrix[i][j]=matrix[j][i];
                 matrix[j][i] = t;
             }
         }
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n/2; j++) {
-                t = matrix[i][j];
-                matrix[i][j] = matrix[i][n-1-j];
-                matrix[i][n-1-j] = t;
-            }
-        } 
-}
+    }
+
 }
